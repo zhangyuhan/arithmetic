6b3a1c141dadb5db338a94375e5b393ad169db6d
zhangyuhan <zhang1988yuhan@163.com>
2020-08-19 22:24:09 +0800

    构建对象

---
+++ arithmetic/ari.java
@@ -0,0 +1,8 @@
+/**
+ * @program: arithmetic
+ * @description:
+ * @author: Devin
+ * @create: 2020-08-19 22:22
+ */
+public class ari {
+}
---
+++ test/Test.java
@@ -0,0 +1,8 @@
+/**
+ * @program: arithmetic
+ * @description:
+ * @author: Devin
+ * @create: 2020-08-19 22:23
+ */
+public class Test {
+}
